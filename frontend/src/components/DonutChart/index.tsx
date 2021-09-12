b6042063aca3b61d5297f1f010f019bb6b290e47
Gabriel Henrique <gcoelho730@gmail.com>
2021-09-12 02:39:38 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -24,11 +24,6 @@ const DonutChart = () => {
             });
     }, [])
 
-    //const mockData = {
-    //    series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
-
     const options = {
         legend: {
             show: true
@@ -50,6 +45,5 @@ const DonutChart = () => {
     );
 
 }
-
 
 export default DonutChart;
